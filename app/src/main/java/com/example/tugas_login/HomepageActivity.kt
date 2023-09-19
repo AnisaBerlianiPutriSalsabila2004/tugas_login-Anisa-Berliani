4ebc67f7ff1ad7a30803efec95a4044bdf70cd6a
anisaberlianiputrisalsabila2004@mail.ugm.ac.id <anisaberlianiputrisalsabila2004@mail.ugm.ac.idgit config --global user.name AnisaBerlianiPutriSalsabila2004>
2023-09-18 20:59:49 -0800

    Initial commit

---
+++ app/src/main/java/com/example/tugas_login/HomepageActivity.kt
@@ -0,0 +1,29 @@
+package com.example.tugas_login
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import com.example.tugas_login.databinding.ActivityHomepageBinding
+
+class HomepageActivity : AppCompatActivity() {
+
+    private lateinit var binding: ActivityHomepageBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        binding = ActivityHomepageBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        val usernames = intent.getStringExtra(RegisterActivity.username)
+        val emails = intent.getStringExtra(RegisterActivity.email)
+        val phones = intent.getStringExtra(RegisterActivity.phone)
+        val passwords = intent.getStringExtra(RegisterActivity.password)
+
+        with(binding){
+
+            txtUsername.text = "Welcome $usernames !!"
+            txtEmail.text = "Your email : $emails"
+            txtPhone.text = "Your phone number : $phones"
+        }
+    }
+}
